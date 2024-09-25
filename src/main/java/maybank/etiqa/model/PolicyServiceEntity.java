3b12c862e2d2e27ed401cf59be28bcc55e5c51a4
sofiyakhal <sofiyakhalisha01@gmail.com>
2024-09-25 16:53:27 +0800

    Renamed service model

---
+++ src/main/java/maybank/etiqa/model/PolicyServiceEntity.java
@@ -0,0 +1,54 @@
+package maybank.etiqa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
+public class PolicyServiceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String serviceType;
+    private LocalDate serviceDate;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name="policy_id")
+    private Policy policy;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getServiceType() {
+        return serviceType;
+    }
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+    public LocalDate getServiceDate() {
+        return serviceDate;
+    }
+    public void setServiceDate(LocalDate serviceDate) {
+        this.serviceDate = serviceDate;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Policy getPolicy() {
+        return policy;
+    }
+    public void setPolicy(Policy policy) {
+        this.policy = policy;
+    }
+}
